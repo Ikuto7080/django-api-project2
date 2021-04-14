f4b9d704d0590e02a5348398c1cd8239e1f04af3
Your Name <ikuto.hongu@gmail.com>
2021-04-15 01:00:52 +0900

    heroku deplay

--- core/views.py
+++ core/views.py
@@ -34,6 +34,7 @@ class LineWebHookView(views.APIView):
                 #send push messages
                 line_bot_api.push_message(user_id, TextSendMessage(text=domain_url + '/login/?user_id=' + user_id + '&account_id=' + str(request_id)))
             except:
+                line_bot_api.push_message(user_id, TextSendMessage(text='Go to this url to sign up!'))
                 line_bot_api.push_message(user_id, TextSendMessage(text=domain_url + '/login/?user_id=' + user_id))
         except Exception as e:
             print(e)
