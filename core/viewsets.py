74ef75b97125d4c85835961fdcff8eb342024a89
Your Name <ikuto.hongu@gmail.com>
2021-06-13 15:45:24 +0900

    get, post

--- core/viewsets.py
+++ core/viewsets.py
@@ -35,7 +35,8 @@ class AccountViewSet(viewsets.ModelViewSet):
         return queryset
 
     def get_permissions(self):
-        if self.request.method == 'GET' or 'POST':
+        METHODS = ['GET', 'POST']
+        if self.request.method == METHODS:
             return [permissions.AllowAny()]
         else:
             return [permissions.IsAuthenticated()]
