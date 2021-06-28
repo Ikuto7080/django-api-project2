8b49b23ea58404b4451a0f9e0b73569f9f8ecb2f
Your Name <ikuto.hongu@gmail.com>
2021-06-29 01:00:16 +0900

    delete fcm_token in accountserializes

--- core/serializers.py
+++ core/serializers.py
@@ -54,8 +54,6 @@ class AccountSerializer(serializers.ModelSerializer):
           follow_account_id = validated_data.get("follow_account_id")
           print('follow_account_id: ', follow_account_id)
           redirect_uri = validated_data.get("redirect_uri", "https://localhost:8080/insta/")
-          apns_token = validated_data.get("apns_token")
-          print("apns_token: ")
 
           if fb_code:
                 url = "https://graph.facebook.com/v9.0/oauth/access_token"
@@ -90,11 +88,6 @@ class AccountSerializer(serializers.ModelSerializer):
                 account.fb_token = fb_token
                 # print('user: ' + str(user))
                 account.save()
-                # Deviceにapns_token保存
-                device = Device()
-                device.fcm_token = apns_token
-                device.account = account
-                device.save()
                 get_fb_post.delay(account.id)
                 if follow_account_id:
                     print('follo_account_id: ', follow_account_id)
