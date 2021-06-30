9d583ff9226fe5e1206672838935ba84c5a8c9d5
lugaojian783 <lugaojian783@gmail.com>
2021-07-01 01:03:25 +0900

    add fb_access_token in serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -30,6 +30,7 @@ class InviterAccountSerializer(serializers.ModelSerializer):
 
 class AccountSerializer(serializers.ModelSerializer):
     fb_code = serializers.CharField(required=False, write_only=True)
+    fb_access_token = serializers.CharField(required=False, write_only=True)
     ig_code = serializers.CharField(required=False, write_only=True)
     account_id = serializers.CharField(required=False,write_only=True)
     follow_account_id = serializers.CharField(required=False, write_only=True)
@@ -39,7 +40,7 @@ class AccountSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Account
-        fields = ['id', 'user', 'fb_token' , 'ig_token', 'fb_code', 'ig_code', 'fb_id', 'ig_id', 'redirect_uri', 'profile_picture', 'account_id', 'follow_account_id', 'inviter']
+        fields = ['id', 'user', 'fb_token' , 'ig_token', 'fb_code', 'ig_code', 'fb_id', 'ig_id', 'redirect_uri', 'profile_picture', 'account_id', 'follow_account_id', 'inviter', 'fb_access_token']
         read_only_fields = ['user', 'fb_token', 'ig_token', 'fb_id', 'ig_id', 'inviter']
 
     def update(self, instance, validated_data):
@@ -47,25 +48,63 @@ class AccountSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
     def create(self, validated_data):
-          print('start')
-          fb_code = validated_data.get("fb_code")
-          ig_code = validated_data.get("ig_code")
-          account_id = validated_data.get("account_id")
-          follow_account_id = validated_data.get("follow_account_id")
-          print('follow_account_id: ', follow_account_id)
-          redirect_uri = validated_data.get("redirect_uri", "https://localhost:8080/insta/")
+        print('start')
+        fb_code = validated_data.get("fb_code")
+        ig_code = validated_data.get("ig_code")
+        fb_access_token = validated_data.get("fb_access_token")
+        account_id = validated_data.get("account_id")
+        follow_account_id = validated_data.get("follow_account_id")
+        print('follow_account_id: ', follow_account_id)
+        redirect_uri = validated_data.get("redirect_uri", "https://localhost:8080/insta/")
 
-          if fb_code:
-                url = "https://graph.facebook.com/v9.0/oauth/access_token"
-                params = {
-                'client_id': '420945845838455',
-                'redirect_uri': redirect_uri,
-                'client_secret': '86f24082416e7e017e2c4f8f4e39458f',
-                'code': fb_code
-                }
-                r = requests.get(url, params=params)
-                form = r.json()
-                fb_token = form['access_token']
+        if fb_code:
+            url = "https://graph.facebook.com/v9.0/oauth/access_token"
+            params = {
+            'client_id': '420945845838455',
+            'redirect_uri': redirect_uri,
+            'client_secret': '86f24082416e7e017e2c4f8f4e39458f',
+            'code': fb_code
+            }
+            r = requests.get(url, params=params)
+            form = r.json()
+            fb_token = form['access_token']
+            token = fb_token
+            graph = facebook.GraphAPI(token)
+            #fields = ['first_name', 'location{location}','email','link']
+            profile = graph.get_object('me', fields='first_name, last_name, location,link,email')
+            #Accountが存在するか確認する
+            fb_id = profile['id'] 
+            account = Account.objects.filter(fb_id=fb_id).first()
+            # print('account: ' + str(account))
+            if account:
+                return account
+            #userのfb_id
+            user = User(username=str(uuid.uuid4()))
+            user.first_name = profile['first_name']
+            user.last_name = profile['last_name']
+            user.email = profile.get('email', '')
+            user.save()
+            account = Account()
+            account.user = user
+            account.fb_id = profile['id']
+            account.fb_token = fb_token
+            # print('user: ' + str(user))
+            account.save()
+            get_fb_post.delay(account.id)
+            if follow_account_id:
+                print('follo_account_id: ', follow_account_id)
+                follow_account = Account.objects.filter(id=follow_account_id).first()
+                print('follow_account: ', follow_account)
+                follow_account.user.profile.friends.add(account.user)
+                account.user.profile.friends.add(follow_account.user)
+                account.inviter = follow_account
+                print('account.inviter: ', account.inviter)
+                account.save()
+                send_notification.delay(account.id, follow_account_id)
+            return account
+
+        elif fb_access_token:
+                fb_token = fb_access_token
                 token = fb_token
                 graph = facebook.GraphAPI(token)
                 #fields = ['first_name', 'location{location}','email','link']
@@ -100,22 +139,23 @@ class AccountSerializer(serializers.ModelSerializer):
                     account.save()
                     send_notification.delay(account.id, follow_account_id)
                 return account
+            
 
-          elif ig_code:
-                instagram_basic_display = InstagramBasicDisplay(app_id ='909807339845904', app_secret='f095f16729ea435ff0c36d6fda438d83', redirect_url= redirect_uri)
-                auth_token = instagram_basic_display.get_o_auth_token(ig_code)
-                instagram_basic_display.set_access_token(auth_token['access_token'])
-                ig_profile = instagram_basic_display.get_user_profile()
-                ig_id = ig_profile['id']
-                account = Account.objects.filter(id=account_id).first()
-                account.ig_id = ig_id
-                account.ig_token = auth_token['access_token']
-                account.save()
-                # get_ig_post(account.id)
-                return account
+        elif ig_code:
+            instagram_basic_display = InstagramBasicDisplay(app_id ='909807339845904', app_secret='f095f16729ea435ff0c36d6fda438d83', redirect_url= redirect_uri)
+            auth_token = instagram_basic_display.get_o_auth_token(ig_code)
+            instagram_basic_display.set_access_token(auth_token['access_token'])
+            ig_profile = instagram_basic_display.get_user_profile()
+            ig_id = ig_profile['id']
+            account = Account.objects.filter(id=account_id).first()
+            account.ig_id = ig_id
+            account.ig_token = auth_token['access_token']
+            account.save()
+            # get_ig_post(account.id)
+            return account
                 
-          else:
-                raise serializers.ValidationError('Be specify fb_token or ig_token')
+        else:
+            raise serializers.ValidationError('Be specify fb_token or ig_token')
 
     def to_representation(self, account):
         output = super().to_representation(account)
