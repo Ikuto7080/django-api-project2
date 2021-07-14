430aafa41766e3b565089a73e3c8a5b75b0b7275
Your Name <ikuto.hongu@gmail.com>
2021-07-14 23:49:01 +0900

    make pre_delete

--- core/signals.py
+++ core/signals.py
@@ -1,10 +1,18 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, Account
 
 
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(pre_delete, sender=Account)
+def pre_delete_account(sender, instance, **kwargs):
+    user = User.objects.get(id=instance.user_id)
+    user.delete()
+
+
